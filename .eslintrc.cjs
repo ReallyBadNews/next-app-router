185f3e4f241891e848998d4b7fc9243048cedfa9
Kenny Elshoff <github@kennyelshoff.com>
2023-05-13 16:47:19 -0400

    init shadcn/ui

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+/* eslint-env node */
+module.exports = {
+  extends: [
+    "next/core-web-vitals",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:@typescript-eslint/strict",
+  ],
+  plugins: ["@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  root: true,
+};
