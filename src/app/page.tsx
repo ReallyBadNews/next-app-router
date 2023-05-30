f9ff37a98eb4ab43cc45095ebf242b48838f423c
Kenny Elshoff <github@kennyelshoff.com>
2023-05-29 23:08:30 -0400

    feat: add toast component (#2)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,9 +19,11 @@ async function getData() {
       },
     }
   );
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
-  // Recommendation: handle errors
+  /**
+   * The return value is *not* serialized
+   * You can return Date, Map, Set, etc.
+   * TODO: handle errors
+   */
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
     throw new Error("Failed to fetch data");
